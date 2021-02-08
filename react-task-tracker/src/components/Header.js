2e250b6ff395d2bf83474d6846287127cd044e4b
Arnab Das <arnab.das@orchids.edu.in>
2021-02-08 12:34:18 +0530

    Initial Commit

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -20,4 +20,11 @@ Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
 
+
+//CSS in JS
+//const headingStyle = {
+//    color: 'red',
+//    backgroundColor: 'black'
+//}
+
 export default Header
