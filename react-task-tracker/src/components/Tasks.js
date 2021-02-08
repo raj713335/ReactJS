1aaa37f9f359db96bf5e770fa4337e17f9fda8fd
Arnab Das <arnab.das@orchids.edu.in>
2021-02-08 18:10:56 +0530

    Initial Commit

--- react-task-tracker/src/components/Tasks.js
+++ react-task-tracker/src/components/Tasks.js
@@ -1,33 +1,12 @@
-import { useState } from 'react'
+import Task from './Task'
 
-
-
-const Tasks = () => {
-
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            remainder: true,
-        },
-        {
-            id: 2,
-            text: 'Meeting at School',
-            day: 'Feb 6th at 1:30pm',
-            remainder: true,
-        },
-        {
-            id: 3,
-            text: 'Food Shopping',
-            day: 'Feb 5th at 2:30pm',
-            remainder: false,
-        },
-    ])
+const Tasks = ({ tasks }) => {
 
     return (
         <>
-            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+            {tasks.map((task) => (
+                <Task key={task.id} task={task} />
+            ))}
         </>
     )
 }
