f1c9883de019719a8082271118669b750fffa2ed
Arnab Das <arnab.das@orchids.edu.in>
2021-02-08 11:13:51 +0530

    Initial Commit

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-    <h1> Hello From React</h1>
+        <h1> Hello From React</h1>
+        <h2> Hello</h2>
 
 
     </div>
-  );
+  )
 }
 
 export default App;
