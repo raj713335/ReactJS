52d734c51be30f06a5c9d450075e13ae1b0c6cb7
Arnab Das <arnab.das@orchids.edu.in>
2021-02-08 12:00:25 +0530

    Initial Commit

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,8 +1,11 @@
 function App() {
+  const name = 'Brad'
+  const x = true
+
   return (
     <div className="App">
         <h1> Hello From React</h1>
-        <h2> Hello</h2>
+        <h2> Hello {name} </h2>
 
 
     </div>
---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <h1> Task Tracker </h1>
+            </header>
+            
+        </div>
+    )
+}
+
+export default Header
