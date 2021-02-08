d510872ce8911f3cebe5cc7afeb54ddefa1b9196
Arnab Das <arnab.das@orchids.edu.in>
2021-02-09 00:49:05 +0530

    Initial Commit

--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -1,8 +1,9 @@
+import { FaTimes } from 'react-icons/fa'
 
 const Task = ({ task }) => {
     return (
         <div className='task'>
-            <h3>{ task.text }</h3>
+            <h3>{ task.text } <FaTimes /></h3>
             <p>{ task.day }</p>
         </div>
         )
