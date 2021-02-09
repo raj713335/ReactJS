70e0781fd6fd362f227f7b7e35f3fe9ec2c1d9aa
Arnab Das <arnab.das@orchids.edu.in>
2021-02-09 15:48:27 +0530

    Initial Commit

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -22,27 +22,67 @@ const App = () => {
 
     // Fetch Tasks
     const fetchTasks = async() => {
-            const res = await fetch('http://localhost:5000/tasks')
+            const res = await fetch(`http://localhost:5000/tasks`)
+            const data = await res.json()
+
+            return data
+            }
+
+
+    // Fetch Task
+    const fetchTask = async(id) => {
+            const res = await fetch(`http://localhost:5000/tasks/${id}`)
             const data = await res.json()
 
             return data
             }
 
     // Add Task
-    const addTask = (task) => {
-        const id = Math.floor(Math.random()*10000)+1
-        const newTask = {id, ...task }
-        setTasks([...tasks,newTask])
+    const addTask = async (task) => {
+        //        const id = Math.floor(Math.random()*10000)+1
+        //        const newTask = {id, ...task }
+        //        setTasks([...tasks,newTask])
+
+        const res = await fetch(`http://localhost:5000/tasks`, {
+        method: 'POST',
+        headers: {
+        'Content-type': 'application/json',
+        },
+        body: JSON.stringify(task),
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+
+        //await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
     }
 
     //Delete Task
-    const deleteTask = (id) => {
+    const deleteTask = async (id) => {
+        await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
         setTasks(tasks.filter((task) => task.id !==id))
     }
 
     // Toggle Remainder
-    const toggleRemainder = (id) => {
-        setTasks(tasks.map((task) => task.id === id ? { ...task,reminder: !task.reminder } :task))
+    const toggleRemainder = async (id) => {
+
+        const taskToToggle = await fetchTask(id)
+        const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+        const res = await fetch(`http://localhost:5000/tasks/${id}`, { method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updTask)
+        })
+
+        const data = await res.json()
+
+        setTasks(tasks.map((task) => task.id === id ? { ...task,reminder: data.reminder } :task))
     }
 
 
