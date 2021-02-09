f6945a56591579ef4fd441275ac63ab5c4cf5b58
Arnab Das <arnab.das@orchids.edu.in>
2021-02-09 12:15:10 +0530

    Initial Commit

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -6,6 +6,8 @@ import AddTask from './components/AddTask'
 
 const App = () => {
 
+    const [showAddTask, setShowAddTask] = useState(false)
+
     const [tasks, setTasks] = useState([
         {
             id: 1,
@@ -47,8 +49,8 @@ const App = () => {
 
     return (
         <div className="container">
-            <Header />
-            <AddTask onAdd={addTask} />
+            <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+            {showAddTask && <AddTask onAdd={addTask} />}
             {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleRemainder} /> : 'No Tasks to Show'}
 
         </div>
