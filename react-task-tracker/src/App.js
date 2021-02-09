ebd389dcf4d38e2edecc360d0dd1fbd7547c5a42
Arnab Das <arnab.das@orchids.edu.in>
2021-02-09 12:05:31 +0530

    Initial Commit

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -29,7 +29,9 @@ const App = () => {
 
     // Add Task
     const addTask = (task) => {
-        console.log(task)
+        const id = Math.floor(Math.random()*10000)+1
+        const newTask = {id, ...task }
+        setTasks([...tasks,newTask])
     }
 
     //Delete Task
