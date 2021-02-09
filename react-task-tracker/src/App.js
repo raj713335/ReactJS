e3933933339d527378e03f9b585dd787d26aaf24
Arnab Das <arnab.das@orchids.edu.in>
2021-02-09 17:02:08 +0530

    Initial Commit

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import About from './components/About'
+
 
 
 const App = () => {
@@ -87,12 +91,20 @@ const App = () => {
 
 
     return (
+        <Router>
         <div className="container">
             <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-            {showAddTask && <AddTask onAdd={addTask} />}
-            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleRemainder} /> : 'No Tasks to Show'}
 
+            <Route path='/' exact render={(props) =>
+                (
+                <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleRemainder} /> : 'No Tasks to Show'}
+                </>)} />
+            <Route path='/about' component={About} />
+            <Footer />
         </div>
+        </Router>
       )
 }
 
